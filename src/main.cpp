18a7ca21015cdc17e99536bb5fc47d9a85d6f6ce
TomasBeranek <xberan46@stud.fit.vutbr.cz>
2022-05-04 22:58:26 +0200

    Add generations to config

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ int main(int argc, char const *argv[]) {
   int elitism =             config["Elitism"].asInt();
   int parentPopulation =    config["ParentPopulation"].asInt();
   int mutationPercetange =  config["MutationPercetange"].asInt();
+  int generations =         config["Generations"].asInt();
 
   vector<GameOfLife> population;
 
