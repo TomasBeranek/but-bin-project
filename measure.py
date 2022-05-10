4da7bc8de62edaf24510417063e9528e2c892a1a
TomasBeranek <xberan46@stud.fit.vutbr.cz>
2022-05-11 01:24:17 +0200

    Measure real time, since we are using parallelism

--- measure.py
+++ measure.py
@@ -6,10 +6,10 @@ def getUpdatedCells(command):
     o, e = subprocess.Popen(['bash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
     time_output = e.decode('utf-8')
-    user_time = time_output.split('\n')[2].split('\t')[1]
-    user_m = float(user_time.split('m')[0])
-    user_s = float(user_time.split('m')[1][:-1].replace(',', '.'))
-    total_s = user_m*60 + user_s
+    real_time = time_output.split('\n')[1].split('\t')[1]
+    real_m = float(real_time.split('m')[0])
+    real_s = float(real_time.split('m')[1][:-1].replace(',', '.'))
+    total_s = real_m*60 + real_s
 
     # For testing:
     # {
